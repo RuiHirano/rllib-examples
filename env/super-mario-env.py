f21e1c30b15d90e2a15b7a3cda66d1d166c56fbb
平野流 <ruihirano@hiranoryuus-MacBook-Pro.local>
2022-11-23 00:27:42 +0900

    fix super mario env

--- env/super-mario-env.py
+++ env/super-mario-env.py
@@ -2,12 +2,27 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
+from gym.spaces import Box
+import numpy as np
+from PIL import Image
 
-class SuperMarioBrosEnv(gym.Wrapper):
+class SuperMarioBrosEnv(gym.ObservationWrapper):
     def __init__(self):
         env = gym_super_mario_bros.make('SuperMarioBros-v0')
         env = JoypadSpace(env, SIMPLE_MOVEMENT)
+        env.observation_space = Box(
+            low=0,
+            high=255,
+            shape=(84, 84, 3),
+            dtype=np.uint8
+        )
         super(SuperMarioBrosEnv, self).__init__(env)
+
+    def observation(self, obs):
+        img = Image.fromarray(np.array(obs))
+        img_resize = img.resize((84, 84))
+        obs = np.array(img_resize)
+        return obs
 
 if __name__ == "__main__":
     env = SuperMarioBrosEnv()
