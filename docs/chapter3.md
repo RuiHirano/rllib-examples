0187a27cce0b9127ae674c7d9129fd928b836ee1
平野流 <ruihirano@hiranoryuus-MacBook-Pro.local>
2022-11-22 21:56:46 +0900

    add super mario env

--- docs/chapter3.md
+++ docs/chapter3.md
@@ -1,3 +1,15 @@
 # Chapter3 -  カスタマイズされたEnvを利用する
 
-## 1. Comming soon
+## 1. Super Mario Envの作成
+以下のコマンドでsuper-marioを実行し動作を確認します。
+```
+./run python env/super-mario-env.py
+```
+以下のような出力であれば問題ないです。
+```
+step: 4996, reward: 0.0, done: False, info: {'coins': 0, 'flag_get': False, 'life': 2, 'score': 0, 'stage': 1, 'status': 'small', 'time': 151, 'world': 1, 'x_pos': 594, 'y_pos': 89}
+step: 4997, reward: 0.0, done: False, info: {'coins': 0, 'flag_get': False, 'life': 2, 'score': 0, 'stage': 1, 'status': 'small', 'time': 151, 'world': 1, 'x_pos': 594, 'y_pos': 93}
+step: 4998, reward: -1.0, done: False, info: {'coins': 0, 'flag_get': False, 'life': 2, 'score': 0, 'stage': 1, 'status': 'small', 'time': 150, 'world': 1, 'x_pos': 594, 'y_pos': 97}
+step: 4999, reward: 0.0, done: False, info: {'coins': 0, 'flag_get': False, 'life': 2, 'score': 0, 'stage': 1, 'status': 'small', 'time': 150, 'world': 1, 'x_pos': 594, 'y_pos': 100}
+...
+```
--- env/super-mario-env.py
+++ env/super-mario-env.py
@@ -1,14 +1,23 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+import gym
 
-done = True
-for step in range(5000):
-    if done:
-        state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-    env.render()
+class SuperMarioBrosEnv(gym.Wrapper):
+    def __init__(self):
+        env = gym_super_mario_bros.make('SuperMarioBros-v0')
+        env = JoypadSpace(env, SIMPLE_MOVEMENT)
+        super(SuperMarioBrosEnv, self).__init__(env)
 
-env.close()
+if __name__ == "__main__":
+    env = SuperMarioBrosEnv()
+
+    done = True
+    for step in range(5000):
+        if done:
+            state = env.reset()
+        state, reward, done, info = env.step(env.action_space.sample())
+        print("step: {}, reward: {}, done: {}, info: {}".format(step, reward, done, info))
+        #env.render()
+
+    env.close()
